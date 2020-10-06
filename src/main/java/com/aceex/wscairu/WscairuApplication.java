51fb6785f6d4673d08bf93674619b4f2fde3ce0d
ivo.hb <ivohb.me@gmail.com>
2020-10-06 09:55:47 -0300

    criação do projeto

---
+++ src/main/java/com/aceex/wscairu/WscairuApplication.java
@@ -0,0 +1,13 @@
+package com.aceex.wscairu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WscairuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WscairuApplication.class, args);
+	}
+
+}
